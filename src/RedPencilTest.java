1610735aafd06dbec885419e702e0d05a424d461
Mike Mueller <mmueller@pillartechnology.com>
2016-01-14 12:46:36 -0500

    create whenPriceIsReducedThePriceChanges

--- src/RedPencilTest.java
+++ src/RedPencilTest.java
@@ -42,6 +42,28 @@ public class RedPencilTest {
         Assert.assertEquals("Coat (promotion)", item.toString());
     }
 
+    @Test
+    public void whenPriceIsReducedBy5Point4PercentThePromotionBegins(){
+        //Arrange
+        Item item = new Item("Coat", 5.00);
 
+        //Act
+        item.reducePrice(0.26);
+
+        //Assert
+        Assert.assertEquals("Coat (promotion)", item.toString());
+    }
+
+    @Test
+    public void whenPriceIsReducedThePriceChanges(){
+        //Arrange
+        Item item = new Item("Coat", 5.00);
+
+        //Act
+        item.reducePrice(0.26);
+
+        //Assert
+        Assert.assertEquals(4.74, item.getPrice());
+    }
 
 }
