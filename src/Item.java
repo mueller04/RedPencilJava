0b89505295785a2e0be6ff4b5b5a27ceeaa0d38f
Mike Mueller <mmueller@pillartechnology.com>
2016-01-14 12:14:56 -0500

    passed whenThePriceIsReducedBy4Point99PercentThePromotionDoesNotBegin

--- src/Item.java
+++ src/Item.java
@@ -1,13 +1,19 @@
 public class Item {
 
     public String displayText;
+    public Double price;
 
-    public Item(String displayText){
+    public Item(String displayText, double price){
         this.displayText = displayText;
+        this.price = price;
     }
 
     public void beginPromotion(){
         this.displayText = displayText + " " + "(promotion)";
+    }
+
+    public void reducePrice(double price) {
+        this.price -= price;
     }
 
     @Override
--- src/RedPencilTest.java
+++ src/RedPencilTest.java
@@ -8,15 +8,26 @@ public class RedPencilTest {
     @Test
     public void whenanItemisMarkedAsAPromotionItReturnsPromotionInTheString(){
         //Arrange
-        Item item = new Item("Coat");
+        Item item = new Item("Coat", 5.00);
 
         //Act
         item.beginPromotion();
 
         //Assert
         Assert.assertEquals("Coat (promotion)", item.toString());
-
-
     }
+
+    @Test public void whenThePriceIsReducedBy4Point99PercentThePromotionDoesNotBegin(){
+        //Arrange
+        Item item = new Item("Coat", 5.00);
+
+        //Act
+        item.reducePrice(0.24);
+
+        //Assert
+        Assert.assertEquals("Coat", item.toString());
+    }
+
+    
 
 }
