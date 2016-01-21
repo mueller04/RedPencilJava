8e015aafc29fae0b4c8dded84b5b75aa99931485
Mike Mueller <mmueller@pillartechnology.com>
2016-01-21 16:03:35 -0500

    adding file Promotion.java

---
+++ src/Promotion.java
@@ -0,0 +1,39 @@
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class Promotion {
+
+    public boolean isPromotion = false;
+    public String promotionText = "";
+    public LocalDate promotionBeginDate;
+
+    public Promotion() {
+        isPromotion = true;
+        promotionText = " (promotion)";
+        promotionBeginDate = LocalDate.now();
+    }
+
+    public LocalDate getPromotionBeginDate() {return promotionBeginDate;}
+
+    public boolean getPromotion() {return isPromotion;}
+
+    public void expirePromotionIfAged() {
+        if (promotionBeginDate != null) {
+            LocalDate now = LocalDate.now();
+            if (ChronoUnit.DAYS.between(promotionBeginDate, now) > 30) {
+                expirePromotion();
+            }
+        }
+    }
+
+    public void expirePromotion() {
+        isPromotion = false;
+        promotionText = "";
+        promotionBeginDate = null;
+    }
+
+    //Test Methods
+    public void setBeginDateForTest(LocalDate testBeginDate) {
+        this.promotionBeginDate = testBeginDate;
+    }
+}
